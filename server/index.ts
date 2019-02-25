a752298a5af2c5de4aa42bfc8e255fdbaa6d63b7
Andrew H <aho338@gmail.com>
2019-02-24 22:44:34 -0800

    Fix routes, prettier

--- server/index.ts
+++ server/index.ts
@@ -1,36 +1,41 @@
-import express from "express";
-import * as bodyParser from "body-parser";
-import { prisma } from "./prisma/generated/prisma-client";
+import * as path from 'path';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import { prisma } from './prisma/generated/prisma-client';
 
 const app = express();
 
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.disable("x-powered-by");
+app.disable('x-powered-by');
 
-app.set("port", process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3001);
 
-app.get("/talks", async (req, res) => {
+app.get('/talks', async (req, res) => {
   const talks = await prisma.talks();
   res.json(talks);
 });
 
-app.get("/speakers", async (req, res) => {
-  const speakers = await prisma.talks();
+app.get('/speakers', async (req, res) => {
+  const speakers = await prisma.speakers();
   res.json(speakers);
 });
 
-app.get("/events", async (req, res) => {
-  const events = await prisma.talks();
+app.get('/events', async (req, res) => {
+  const events = await prisma.events();
   res.json(events);
 });
 
-app.listen(app.get("port"), () => {
-  if (process.env.NODE_ENV !== "production") {
-    console.log(`Find the server at: http://localhost:${app.get("port")}/`);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.listen(app.get('port'), () => {
+  if (process.env.NODE_ENV !== 'production') {
+    console.log(`Find the server at: http://localhost:${app.get('port')}/`);
   }
 });
