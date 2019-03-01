9ef41cd9fe7293700b2649b1485465b5c1d71e2e
Andrew H <aho338@gmail.com>
2019-03-01 10:02:02 -0800

    Make indexed fields explicit, format publishedAt

--- scripts/createAlgoliaIndex.ts
+++ scripts/createAlgoliaIndex.ts
@@ -1,6 +1,5 @@
 import * as algoliasearch from 'algoliasearch';
 import { prisma } from '../prisma/generated/prisma-client';
-import { omit } from 'lodash';
 
 if (!process.env['ALGOLIA_APP_ID'] || !process.env['ALGOLIA_API_KEY']) {
   throw 'Missing Algolia credentials.';
@@ -11,7 +10,7 @@ const client = algoliasearch(
   process.env['ALGOLIA_API_KEY'] || ''
 );
 
-const talksIndex = client.initIndex('prod_TALKS');
+const talksIndex = client.initIndex('TALKS');
 
 async function createTalksAlgoliaIndex() {
   const talks = await prisma.talks({
@@ -19,13 +18,17 @@ async function createTalksAlgoliaIndex() {
       private: false
     }
   });
-  const mappedTalks = talks.map(talk => {
-    const objectID = talk.id;
-    return {
-      ...omit(talk, 'id'),
-      objectID
-    };
-  });
+  const mappedTalks = talks.map(talk => ({
+    viewCount: talk.viewCount,
+    description: talk.description,
+    title: talk.title,
+    duration: talk.duration,
+    source: talk.source,
+    videoId: talk.videoId,
+    thumbnailUrl: talk.thumbnailUrl,
+    publishedAt: talk.publishedAt ? new Date(talk.publishedAt).valueOf() : null,
+    objectID: talk.id
+  }));
 
   talksIndex.addObjects(mappedTalks, (err, _content) => {
     if (err) {
