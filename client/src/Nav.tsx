0f7f0c91601b399e4d399649e87ea48f86d755a3
Andrew H <aho338@gmail.com>
2019-04-10 18:51:20 -0700

    Add Search Box toggling to Nav

--- client/src/Nav.tsx
+++ client/src/Nav.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import { color, space } from 'styled-system';
 import { SearchBox } from 'react-instantsearch-dom';
 import { Match } from '@reach/router';
 
 import { NAV_HEIGHT } from './theme';
-import { Box } from './design';
+import { Box, Button } from './design';
 import Logo from './Logo';
 import AlgoliaLogo from './AlgoliaLogo';
+import Icon, { search, close } from './Icon';
 
 const StyledNav = styled('nav')`
   display: flex;
@@ -18,31 +19,86 @@ const StyledNav = styled('nav')`
   ${space}
 `;
 
+const NavButton = styled(Button)`
+  background: none;
+  &:hover {
+    background: none;
+  }
+  ${color};
+`;
+
+const SearchContainer = styled(Box)`
+  display: flex;
+  align-items: center;
+  border-radius: 4px;
+  width: 100%;
+`;
+
+function HomeNav() {
+  const [isSearchOpen, toggleSearchOpen] = useState();
+  const handleCloseSearch = (e: KeyboardEvent) => {
+    const which = e.which || e.keyCode;
+    if (which === 27) {
+      // Escape key
+      toggleSearchOpen(false);
+    }
+  };
+  useEffect(() => {
+    if (isSearchOpen) {
+      document.addEventListener('keydown', handleCloseSearch);
+    }
+    return () => {
+      document.removeEventListener('keydown', handleCloseSearch);
+    };
+  }, [isSearchOpen]);
+
+  return isSearchOpen ? (
+    <SearchContainer color="gray" p={1}>
+      <SearchBox
+        translations={{
+          placeholder: 'Find a talk...'
+        }}
+        submit={<Icon path={search.path} viewBox={search.viewBox} />}
+        autoFocus={true}
+      />
+      <NavButton
+        p={1}
+        onClick={() => {
+          toggleSearchOpen(false);
+        }}
+        color="gray"
+      >
+        <Icon path={close.path} viewBox={close.viewBox} />
+      </NavButton>
+      <Box p={1}>
+        <a
+          href="https://www.algolia.com"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <AlgoliaLogo />
+        </a>
+      </Box>
+    </SearchContainer>
+  ) : (
+    <React.Fragment>
+      <Logo />
+      <NavButton
+        onClick={() => {
+          toggleSearchOpen(true);
+        }}
+      >
+        <Icon path={search.path} viewBox={search.viewBox} />
+      </NavButton>
+    </React.Fragment>
+  );
+}
+
 function Nav() {
   return (
     <StyledNav bg="black" p={2}>
-      <Logo />
       <Match path="/talks/:objectId">
-        {({ match }) =>
-          match ? null : (
-            <Box display="flex" flexDirection="column">
-              <SearchBox
-                translations={{
-                  placeholder: 'Find a talk...'
-                }}
-              />
-              <Box pt={1}>
-                <a
-                  href="https://www.algolia.com"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  <AlgoliaLogo />
-                </a>
-              </Box>
-            </Box>
-          )
-        }
+        {({ match }) => (match ? <Logo /> : <HomeNav />)}
       </Match>
     </StyledNav>
   );
