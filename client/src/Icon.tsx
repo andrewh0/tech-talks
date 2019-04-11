afae87ac9cc6c88eb113af0b812b48eadfa53dfd
Andrew H <aho338@gmail.com>
2019-04-11 14:57:11 -0700

    Add Menu icon

--- client/src/Icon.tsx
+++ client/src/Icon.tsx
@@ -19,6 +19,17 @@ const search = {
     'M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z'
 };
 
+const menu = {
+  viewBox: '0 0 24 24',
+  path: 'M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z'
+};
+
+const openInNew = {
+  viewBox: '0 0 24 24',
+  path:
+    'M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z'
+};
+
 const SVG = styled('svg')`
   width: ${props => props.width || `24px`};
   height: ${props => props.height || `24px`};
@@ -46,6 +57,6 @@ function Icon(props: {
   );
 }
 
-export { expand, close, search };
+export { menu, openInNew, expand, close, search };
 
 export default Icon;
