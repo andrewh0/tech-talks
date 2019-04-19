54090cdb93de7331c76efb47d8a9b6c3e04dcf5d
Andrew H <aho338@gmail.com>
2019-04-18 18:26:38 -0700

    Fix client path

--- backend/index.ts
+++ backend/index.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, './client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.disable('x-powered-by');
@@ -37,7 +37,7 @@ app.get('/api/talks/:objectId', async (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 }
 
