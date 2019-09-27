3fcb0e7d412ee56aa638573005129c56a0614f51
Andrew H <andrew.leean.ho@gmail.com>
2019-09-26 21:54:08 -0700

    Remove firebase types from util

--- client/src/util.tsx
+++ client/src/util.tsx
@@ -1,52 +1,6 @@
 import { useRef, useEffect, useState } from 'react';
 import { debounce } from 'lodash';
 
-type FirebaseTalk = {
-  createdAt: string;
-  description: string;
-  duration: number;
-  eventId: string;
-  hidden: boolean;
-  id: string;
-  private: boolean;
-  publishedAt: string;
-  source: string;
-  thumbnailUrl: string;
-  title: string;
-  updatedAt: string;
-  videoId: string;
-  viewCount: number;
-};
-
-type FirebaseEvent = {
-  city: string;
-  country: string;
-  createdAt: string;
-  endDate: string;
-  id: string;
-  name: string;
-  organizationId: string;
-  startDate: string;
-  talks: {
-    [id: string]: boolean | null;
-  };
-  type: 'CONFERENCE' | 'MEETUP' | null;
-  updatedAt: string;
-  youtubePaylist: string;
-};
-
-type FirebaseOrganization = {
-  events: {
-    [id: string]: boolean | null;
-  };
-  id: string;
-  name: string;
-  twitterHandle: string;
-  updatedAt: string;
-  website: string;
-  youtubeChannel: string | null;
-};
-
 function usePrevious(value: any) {
   const ref = useRef();
   useEffect(() => {
