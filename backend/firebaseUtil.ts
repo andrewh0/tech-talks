a59a0db6e7f42a7afa0973287f548fca4aa909f9
Andrew H <andrew.leean.ho@gmail.com>
2019-10-02 14:09:42 -0700

    Fix key format

--- backend/firebaseUtil.ts
+++ backend/firebaseUtil.ts
@@ -100,7 +100,7 @@ if (
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     projectId: process.env.FIREBASE_PROJECT_ID
   })
