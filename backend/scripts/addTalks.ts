8a4a0ae5cece8ecf239f6f2160c24cdbddc9cab1
Andrew H <andrew.leean.ho@gmail.com>
2019-10-02 13:46:42 -0700

    Remove comments

--- backend/scripts/addTalks.ts
+++ backend/scripts/addTalks.ts
@@ -1,12 +1,11 @@
 import * as algoliasearch from 'algoliasearch';
-import * as cuid from 'cuid';
 import { getVideoDetails } from './util';
 import db, {
   FirebaseTalk,
   getFirebaseDoc,
-  createTimestamp,
   getOrgById,
-  getEventById
+  getEventById,
+  createTalk
 } from '../firebaseUtil';
 
 /*
@@ -33,22 +32,6 @@ async function addNewTalksToExistingEvent(
   youtubeVideoIds: Array<string>,
   eventId: string
 ) {
-  /*
-    Let skippedTalks
-
-    if event doesn't exist, return
-    otherwise, fetch the org name and store it
-
-    For each video id in youtubeVideoIds, check its existence in talks collection
-      if doesn't exist
-        create new talk document
-        attach it to the event document
-        add it to algolia
-
-    console.log(skipped talks)
-
-  */
-
   const skippedTalks: Array<string> = [];
 
   const event = await getEventById(eventId);
@@ -60,7 +43,7 @@ async function addNewTalksToExistingEvent(
       .where('videoId', '==', vId)
       .get();
     const talk = getFirebaseDoc<FirebaseTalk>(talkDoc);
-    if (!!talk) {
+    if (talk) {
       console.log(
         `Talk with videoId ${vId} exists in event with id ${
           talk.eventId
@@ -74,108 +57,32 @@ async function addNewTalksToExistingEvent(
     const videoDetails = (await getVideoDetails([vId]))[0];
     if (!videoDetails) {
       console.log(`Could not find details on video id: ${vId}. Skipping.`);
-      skippedTalks.push(talk.id);
       return;
     }
-    const talkId = cuid();
-    const now = createTimestamp();
 
-    const createdTalk: FirebaseTalk = {
+    const createdTalk = await createTalk({
       ...videoDetails,
-      eventId: eventId,
-      updatedAt: now,
-      createdAt: now,
-      id: talkId
-    };
+      eventId
+    });
 
-    await db
-      .collection('talks')
-      .doc(talkId)
-      .set(createdTalk);
-    console.log(createdTalk);
-
-    // await db.collection('events').doc(eventId).update({
-    //   [`talks.${talkId}`]: true,
-    //   updatedAt: now
-    // });
-
-    // talksIndex.addObject({
-    //   viewCount: createdTalk.viewCount,
-    //   description: createdTalk.description,
-    //   title: createdTalk.title,
-    //   duration: createdTalk.duration,
-    //   source: createdTalk.source,
-    //   videoId: createdTalk.videoId,
-    //   thumbnailUrl: createdTalk.thumbnailUrl,
-    //   publishedAt: createdTalk.publishedAt
-    //     ? new Date(createdTalk.publishedAt).valueOf()
-    //     : null,
-    //   objectID: createdTalk.id,
-    //   organizationName,
-    //   organizationId
-    // });
+    talksIndex.addObject({
+      viewCount: createdTalk.viewCount,
+      description: createdTalk.description,
+      title: createdTalk.title,
+      duration: createdTalk.duration,
+      source: createdTalk.source,
+      videoId: createdTalk.videoId,
+      thumbnailUrl: createdTalk.thumbnailUrl,
+      publishedAt: createdTalk.publishedAt
+        ? new Date(createdTalk.publishedAt).valueOf()
+        : null,
+      objectID: createdTalk.id,
+      organizationName,
+      organizationId
+    });
   });
-  console.log('Done. Skipped videoIds:', skippedTalks);
-
-  // const allTalksSnapshot
-  // const existingTalks = await prisma.talks({
-  //   where: {
-  //     videoId: youtubeVideoId
-  //   }
-  // });
-  // if (existingTalks.length > 0) {
-  //   console.log('Talk(s) with that videoId already exist:', existingTalks);
-  //   return;
-  // }
-  // const event = (await prisma.event({
-  //   id: eventId
-  // }).$fragment(`
-  //   fragment EventWithOrgs on Event {
-  //     id
-  //     organization {
-  //       id
-  //       name
-  //     }
-  //   }
-  // `)) as { id: string; organization: { id: string; name: string } } | null;
-
-  // if (!event) {
-  //   console.log(`Event with id ${eventId} doesn't exist.`);
-  //   return;
-  // }
-  // const organizationName = event.organization.name;
-  // const organizationId = event.organization.id;
-  // const videoDetails = (await getVideoDetails([youtubeVideoId]))[0];
-
-  // if (!videoDetails) {
-  //   console.log(`Could not find details on video id: ${youtubeVideoId}.`);
-  //   return;
-  // }
-  // const createdTalk = await prisma.createTalk({
-  //   ...videoDetails,
-  //   event: {
-  //     connect: { id: eventId }
-  //   }
-
-  //   // ... createdAt
-  //   // ... updatedAt
-  // });
-
-  // talksIndex.addObject({
-  //   viewCount: createdTalk.viewCount,
-  //   description: createdTalk.description,
-  //   title: createdTalk.title,
-  //   duration: createdTalk.duration,
-  //   source: createdTalk.source,
-  //   videoId: createdTalk.videoId,
-  //   thumbnailUrl: createdTalk.thumbnailUrl,
-  //   publishedAt: createdTalk.publishedAt
-  //     ? new Date(createdTalk.publishedAt).valueOf()
-  //     : null,
-  //   objectID: createdTalk.id,
-  //   organizationName,
-  //   organizationId
-  // });
+  console.log(
+    'Done. Skipped videoIds:',
+    skippedTalks.length ? skippedTalks : 'none'
+  );
 }
-
-// addNewTalksToExistingEvent()
